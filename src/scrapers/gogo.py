06bca20553bf9b63cc525196b9883f95b70de9c8
Zai <122824602+Zai-Kun@users.noreply.github.com>
2023-11-14 01:21:52 +0500

    Fix missing comma in REGISTERED_ACCOUNT_EMAILS list

--- src/scrapers/gogo.py
+++ src/scrapers/gogo.py
@@ -19,7 +19,7 @@ GOGO_HOME_URL = 'https://gogoanimehd.io'
 DUB_EXTENSION = ' (Dub)'
 REGISTERED_ACCOUNT_EMAILS = ['benida7218@weirby.com', 'hareki4411@wisnick.com', 'nanab67795@weirby.com', 'xener53725@weirby.com', 'nenado3105@weirby.com',
                              'yaridod257@weirby.com', 'ketoh33964@weirby.com', 'kajade1254@wisnick.com', 'nakofe3005@weirby.com', 'gedidij506@weirby.com',
-                             'sihaci1525@undewp.com', 'lorimob952@soebing.com', 'nigeha6048@undewp.com', 'goriwij739@undewp.com', 'pekivik280@soebing.com'
+                             'sihaci1525@undewp.com', 'lorimob952@soebing.com', 'nigeha6048@undewp.com', 'goriwij739@undewp.com', 'pekivik280@soebing.com',
                              'hapas66158@undewp.com', 'wepajof522@undewp.com', 'semigo1458@undewp.com', 'xojecog864@undewp.com', 'lobik97135@wanbeiz.com']
 
 SESSION_COOKIES: RequestsCookieJar | None = None
