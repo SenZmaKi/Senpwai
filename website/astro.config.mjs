6f0f7dceb7c8d5157fd98e1603fe3b186873d52e
Sen ZmaKi <phidelalvin@gmail.com>
2026-08-11 10:48:33 +0300

    Polish website

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: import.meta.env.PUBLIC_SITE_URL,
+  trailingSlash: 'never',
   vite: {
     plugins: [tailwindcss()]
   }
