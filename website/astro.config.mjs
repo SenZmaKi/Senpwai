009815a84dac69a397e697afe0d97b2a77e2fb4e
Sen ZmaKi <phidelalvin@gmail.com>
2026-08-12 09:25:50 +0300

    Restore website styles

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 const isDevServer = process.argv.includes('dev');
 
@@ -8,4 +10,7 @@ export default defineConfig({
   site: 'https://senzmaki.github.io',
   base: isDevServer ? '/' : '/Senpwai',
   trailingSlash: 'never',
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
