78801c21164ef3fd7c7cf2a235f72a20b57489b3
Sen ZmaKi <phidelalvin@gmail.com>
2026-08-12 09:21:11 +0300

    Fix Pages website build

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,14 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig(({ command }) => ({
   site: 'https://senzmaki.github.io',
   base: command === 'dev' ? '/' : '/Senpwai',
   trailingSlash: 'never',
-  vite: {
-    plugins: [tailwindcss()]
-  }
 }));
