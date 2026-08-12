defffa56d45b6b47b1b47c67a5fa2f9710b10226
Sen ZmaKi <phidelalvin@gmail.com>
2026-08-12 09:23:33 +0300

    Fix Pages base path

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig(({ command }) => ({
+const isDevServer = process.argv.includes('dev');
+
+export default defineConfig({
   site: 'https://senzmaki.github.io',
-  base: command === 'dev' ? '/' : '/Senpwai',
+  base: isDevServer ? '/' : '/Senpwai',
   trailingSlash: 'never',
-}));
+});
