7292858a2ae2106ea64c20ce32e57a4e27c977b0
Sen ZmaKi <phidelalvin@gmail.com>
2026-08-26 11:14:35 +0300

    Fix Linux desktop integration

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -26,6 +26,7 @@ static void my_application_activate(GApplication *application) {
 
   GtkWindow *window =
       GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
+  gtk_window_set_icon_name(window, "com.senpwai.app");
 
   // Use a header bar when running in GNOME as this is the common style used
   // by applications and is the setup most users will be using (e.g. Ubuntu
